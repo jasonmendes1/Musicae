515a47c1c70436dcdf38d9a9ebdf93c752c2cb0a
Luckdude <DEFTFHMYRER>
2019-11-19 16:48:16 +0000

    Teste

--- Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/vistas/RegisterActivity.java
+++ Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/vistas/RegisterActivity.java
@@ -14,5 +14,6 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         Toast.makeText(this, "Oi", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Oi", Toast.LENGTH_SHORT).show();
     }
 }
