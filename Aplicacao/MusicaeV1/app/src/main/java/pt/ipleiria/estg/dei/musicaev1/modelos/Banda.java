743d62396eaf609ca201279b2d199d4a266212ff
Jason Mendes <jason_mendes_1998@hotmail.com>
2019-11-20 12:46:16 +0000

    Model Banda

--- Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/modelos/Banda.java
+++ Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/modelos/Banda.java
@@ -1,4 +1,11 @@
 package pt.ipleiria.estg.dei.musicaev1.modelos;
 
 public class Banda {
+    private int idbanda, logo;
+    private String nome, descricao, localizacao, contacto;
+    private static int autoIncrementID = 1;
+
+    public Banda(String nome, String descricao, String contacto)
+    {
+        this.idbanda = autoIncrementID++;
 }
