a2bb939da8d82780538cf11fa04d84fd59ce2e85
Pedro Alves <pedromiguel.alves8@gmail.com>
2019-11-20 13:51:31 +0000

    Perfile

--- Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/MenuMainActivity.java
+++ Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/MenuMainActivity.java
@@ -3,18 +3,24 @@ package pt.ipleiria.estg.dei.musicaev1;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MenuMainActivity extends AppCompatActivity {
+import pt.ipleiria.estg.dei.musicaev1.vistas.ProfileFragment;
+
+public class MenuMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
     private NavigationView navigationView;
     private DrawerLayout drawer;
     private FragmentManager fragmentManager;
@@ -47,6 +53,11 @@ public class MenuMainActivity extends AppCompatActivity {
         drawer.addDrawerListener(toggle);
 
         carregarCabecalho();
+
+        fragmentManager = getSupportFragmentManager();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        carregamentoFragmentoInicial();
     }
 
     private void carregarCabecalho() {
@@ -55,5 +66,17 @@ public class MenuMainActivity extends AppCompatActivity {
         View view = navigationView.getHeaderView(0);
         TextView textViewUser = view.findViewById(R.id.tvNome);
         textViewUser.setText(username);
+    }
+
+    private void carregamentoFragmentoInicial(){
+        navigationView.setCheckedItem(R.id.nav_profile);
+        Fragment fragment = new ProfileFragment();
+        fragmentManager.beginTransaction().replace(R.id.contentFragment, fragment).commit();
+        setTitle("Profile");
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        return false;
     }
 }
