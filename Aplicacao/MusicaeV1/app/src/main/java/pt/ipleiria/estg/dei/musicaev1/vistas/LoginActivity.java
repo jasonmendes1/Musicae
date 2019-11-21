04475b3e1b826f98e24f5c178b0adeb0d32e802b
Pedro Alves <pedromiguel.alves8@gmail.com>
2019-11-21 18:09:02 +0000

    Login Updates

--- Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/vistas/LoginActivity.java
+++ Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/vistas/LoginActivity.java
@@ -2,42 +2,37 @@ package pt.ipleiria.estg.dei.musicaev1.vistas;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import pt.ipleiria.estg.dei.musicaev1.MenuMainActivity;
 import pt.ipleiria.estg.dei.musicaev1.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText editTextUsername;
-    private EditText editTextPassword;
+    private TextInputLayout textInputUsername;
+    private TextInputLayout textInputPassword;
+    private TextInputEditText editTextUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        textInputUsername = findViewById(R.id.text_input_username);
+        textInputPassword = findViewById(R.id.text_input_password);
         editTextUsername = findViewById(R.id.etUsername);
-        editTextPassword = findViewById(R.id.etPassword);
     }
 
     public void onClickLogin(View view) {
         String username = editTextUsername.getText().toString();
-        String password = editTextPassword.getText().toString();
-
-        /*if(!isEmailValido(email)){
-            editTextEmail.setError(getString(R.string.erro_email));
+        if (!validateUsername() | !validatePassword()) {
             return;
         }
-
-        if(!isPasswordValida(password)){
-            editTextPassword.setError(getString(R.string.erro_password));
-            return;
-        }*/
 
         Intent intent = new Intent(this, MenuMainActivity.class);
         intent.putExtra(MenuMainActivity.CHAVE_USERNAME, username);
@@ -45,17 +40,30 @@ public class LoginActivity extends AppCompatActivity {
         finish();
     }
 
-    public boolean isEmailValido(String email){
-        if(email == null){
+    private boolean validateUsername() {
+        String usernameInput = editTextUsername.getText().toString().trim();
+
+        if (usernameInput.isEmpty()) {
+            textInputUsername.setError("Field can't be empty");
             return false;
+        } else if (usernameInput.length() > 15) {
+            textInputUsername.setError("Username too long");
+            return false;
+        } else {
+            textInputUsername.setError(null);
+            return true;
         }
-        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
-    public boolean isPasswordValida(String password){
-        if(password == null) {
+    private boolean validatePassword() {
+        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
+
+        if (passwordInput.isEmpty()) {
+            textInputPassword.setError("Field can't be empty");
             return false;
+        } else {
+            textInputPassword.setError(null);
+            return true;
         }
-        return password.length() > 4;
     }
 }
