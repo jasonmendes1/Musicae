9cbbb1466435fb58bfa50a848e3ae0bf6751524b
Jason Mendes <jason_mendes_1998@hotmail.com>
2019-11-27 13:06:45 +0000

    Adicionado Modelo MusicoHabilidade

---
+++ Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/modelos/MusicoHabilidade.java
@@ -0,0 +1,9 @@
+package pt.ipleiria.estg.dei.musicaev1.modelos;
+
+public class MusicoHabilidade {
+    private String experiencia;
+
+    public MusicoHabilidade(String experiencia) {
+        this.experiencia = experiencia;
+    }
+}
