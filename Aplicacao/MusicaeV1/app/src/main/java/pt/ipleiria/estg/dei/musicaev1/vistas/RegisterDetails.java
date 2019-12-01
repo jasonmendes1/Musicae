6439b1fa03c5d962fc61417c96ff4bc52b276efd
Pedro Alves <pedromiguel.alves8@gmail.com>
2019-12-01 22:48:11 +0000

    register_details

---
+++ Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/vistas/RegisterDetails.java
@@ -0,0 +1,16 @@
+package pt.ipleiria.estg.dei.musicaev1.vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pt.ipleiria.estg.dei.musicaev1.R;
+
+public class RegisterDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_details);
+    }
+}
