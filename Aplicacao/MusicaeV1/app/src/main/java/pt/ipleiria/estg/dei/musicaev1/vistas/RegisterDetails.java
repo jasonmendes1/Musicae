948c727641795a07f439fe9b540ffa645da2b4bb
Pedro Alves <pedromiguel.alves8@gmail.com>
2019-12-07 15:18:10 +0000

    registo xml

--- Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/vistas/RegisterDetails.java
+++ Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/vistas/RegisterDetails.java
@@ -2,15 +2,90 @@ package pt.ipleiria.estg.dei.musicaev1.vistas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import pt.ipleiria.estg.dei.musicaev1.R;
 
 public class RegisterDetails extends AppCompatActivity {
 
+    private Spinner spinner;
+
+    private TextView mDisplayDate;
+    private DatePickerDialog .OnDateSetListener mDateSetListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_details);
+
+        //----------------------------------------------------------------------- Select Gender ------------------------------------------------------------------------------
+        spinner = findViewById(R.id.spinnerGenre);
+
+        List<String> categorias = new ArrayList<>();
+        categorias.add(0, "Select Your Genre");
+        categorias.add(1, "Male");
+        categorias.add(2, "Female");
+        categorias.add(3, "?? Other ??");
+
+        ArrayAdapter<String> dataAdapter;
+        dataAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, categorias);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(parent.getItemAtPosition(position) == "Select Your Genre"){
+
+                }else{
+                    String item = parent.getItemAtPosition(position).toString();
+                    Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //--------------------------------------------------------------------- Select Birth Date ------------------------------------------------------------------------------
+        mDisplayDate = findViewById(R.id.tvDate);
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(RegisterDetails.this, android.R.style.Theme_Holo_Dialog_MinWidth, mDateSetListener, year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month + 1;
+                String date = day + "/" + month + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
     }
 }
