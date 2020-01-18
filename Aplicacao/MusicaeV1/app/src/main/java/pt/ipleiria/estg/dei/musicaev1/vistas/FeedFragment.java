dad78bfcdf9ce904612c1a64c0680469efe6f661
Pedro Alves <pedromiguel.alves8@gmail.com>
2020-01-18 17:32:29 +0000

    feed

--- Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/vistas/FeedFragment.java
+++ Aplicacao/MusicaeV1/app/src/main/java/pt/ipleiria/estg/dei/musicaev1/vistas/FeedFragment.java
@@ -1,13 +1,17 @@
 package pt.ipleiria.estg.dei.musicaev1.vistas;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -15,48 +19,80 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
 import pt.ipleiria.estg.dei.musicaev1.R;
 import pt.ipleiria.estg.dei.musicaev1.adaptadores.ListaFeedAdaptador;
+import pt.ipleiria.estg.dei.musicaev1.listeners.FeedListener;
 import pt.ipleiria.estg.dei.musicaev1.modelos.Feed;
+import pt.ipleiria.estg.dei.musicaev1.modelos.Singleton;
+import pt.ipleiria.estg.dei.musicaev1.utils.FeedJsonParser;
 
 
-public class FeedFragment extends Fragment {
+public class FeedFragment extends Fragment implements FeedListener {
 
     private ArrayList<Feed> listaFeed;
-    private ListView lvListaBandas;
+    private ListView lvListaFeed;
     private SearchView searchView;
+    private SwipeRefreshLayout swipeRefreshLayout;
     private Button buttonFiltro, buttonNome, buttonInstrumento;
     private Integer selecionado = 0;
+    private ListaFeedAdaptador listaFeedAdaptador;
+
+    public static final int RESULT_CODE_VER = 10;
+    public static final int RESULT_CODE_EDITAR = 11;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         setHasOptionsMenu(true);
+
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_feed, container, false);
-        //listaFeed = Singleton.getInstance(getContext()).getBandasFeed();
-        lvListaBandas = rootView.findViewById(R.id.lvFeed);
-/*        lvListaBandas.setAdapter(new ListaFeedAdaptador(getContext(), listaFeed));
 
-        lvListaBandas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        lvListaFeed = rootView.findViewById(R.id.lvListaFeed);
+        lvListaFeed.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Feed tempFeed = (Feed) parent.getItemAtPosition(position);
                 Intent intent = new Intent(getContext(), ProfileBandActivity.class);
-                startActivity(intent);
+                intent.putExtra(ProfileBandActivity.ID_BANDA, tempFeed.getId());
+                startActivityForResult(intent, RESULT_CODE_VER);
             }
         });
+
+        Singleton.getInstance(getContext()).setFeedListener(this);
+        Singleton.getInstance(getContext()).getAllBandasFeedAPI(getContext(), FeedJsonParser.isConnectionInternet(getContext()));
+
+        swipeRefreshLayout = rootView.findViewById(R.id.swipeLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Singleton.getInstance(getContext()).getAllBandasFeedAPI(getContext(), FeedJsonParser.isConnectionInternet(getContext()));
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 2000);
+            }
+        });
+
         buttonFiltro = rootView.findViewById(R.id.btnFiltro);
         buttonFiltro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), FilterActivity.class);
-                startActivity(intent);  
+                startActivity(intent);
             }
         });
-*/
+
         buttonNome = rootView.findViewById(R.id.btnNomeFeed);
         buttonInstrumento = rootView.findViewById(R.id.btnInstrumentoFeed);
 
@@ -125,50 +161,17 @@ public class FeedFragment extends Fragment {
     }
 
     @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        inflater.inflate(R.menu.menu_pesquisa, menu);
-
-        MenuItem itemPesquisa = menu.findItem(R.id.itemPesquisa);
-        searchView = (SearchView) itemPesquisa.getActionView();
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                ArrayList<Feed> tempListaBandas = new ArrayList<>();
-                /*for (Feed tempFeed: Singleton.getInstance(getContext()).getBandasFeed()) {
-                    if(tempFeed.getNome().toLowerCase().contains(newText.toLowerCase())){
-                        tempListaBandas.add(tempFeed);
-                    }
-                }*/
-
-                lvListaBandas.setAdapter(new ListaFeedAdaptador(getContext(), tempListaBandas));
-                return true;
-            }
-        });
-
-        super.onCreateOptionsMenu(menu, inflater);
+    public void onRefreshListaBandasFeed(ArrayList<Feed> listaFeed) {
+        System.out.println("--> onRefreshListaFeed" + listaFeed);
+        if(!listaFeed.isEmpty()){
+            listaFeedAdaptador = new ListaFeedAdaptador(getContext(), listaFeed);
+            lvListaFeed.setAdapter(listaFeedAdaptador);
+            listaFeedAdaptador.refresh(listaFeed);
+        }
     }
 
     @Override
-    public void onResume() {
-        if(searchView != null){
-            searchView.onActionViewCollapsed();
-        }
-        super.onResume();
-    }
+    public void onUpdateListaBandasFeed(Feed feed, int operacao) {
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.itemPesquisa){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
